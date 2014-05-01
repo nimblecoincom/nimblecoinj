a07800d8205576a4e63b0a1df120207345ea0ab0
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2014-05-01 17:51:23 -0300

    Updated target spacing to 5 seconds and retargeting interval to 1 day

--- core/src/main/java/com/google/bitcoin/core/NetworkParameters.java
+++ core/src/main/java/com/google/bitcoin/core/NetworkParameters.java
@@ -121,8 +121,8 @@ public abstract class NetworkParameters implements Serializable {
         return genesisBlock;
     }
 
-    public static final int TARGET_TIMESPAN = 14 * 24 * 60 * 60;  // 2 weeks per difficulty cycle, on average.
-    public static final int TARGET_SPACING = 10 * 60;  // 10 minutes per block.
+    public static final int TARGET_TIMESPAN = 24*60*60;  // 1 day
+    public static final int TARGET_SPACING = 5;  // 5 seconds per block.
     public static final int INTERVAL = TARGET_TIMESPAN / TARGET_SPACING;
     
     /**
