93c3bfc96603355a7d79035a48a85d54c8d2b946
Oscar Guindzberg <oscar.guindzberg@gmail.com>
2014-08-22 16:51:15 -0300

    dump-stalesX.sh: accept stales-period and stales-max parameters

--- tools/netbox/dump-stalesX.sh
+++ tools/netbox/dump-stalesX.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
-if [ "$1" != "" ]; then
-    ./simple-wallet-tool dump-stales --mode=FULL --net=REGTEST --wallet=data/regtest$1.wallet --chain=data/regtest$1.chain --debuglog
+if [ "$2" != "" ]; then
+    STALES_PERIOD='--stales-period='$2
 else
-    echo 'Usage: dump-stalesX.sh node'
+    STALES_PERIOD=''
+fi
+if [ "$3" != "" ]; then
+    STALES_MAX='--stales-max='$3
+else
+    STALES_MAX=''
+fi
+
+if [ "$1" != "" ]; then
+    ./simple-wallet-tool dump-stales --mode=FULL --net=REGTEST --wallet=data/regtest$1.wallet --chain=data/regtest$1.chain --debuglog $STALES_PERIOD $STALES_MAX
+else
+    echo 'Usage: dump-stalesX.sh node [stales-period] [stales-max]'
 fi
